529c33bfcdbb4c41bca80aac93be048901b08183
Alexis <texs.mv@gmail.com>
2019-04-24 19:11:40 -0500

    Tipos de datos

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "tipos.h"
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
---
+++ tipos.h
@@ -0,0 +1,55 @@
+#include <list>
+#include <map>
+#include <vector>
+#include <tuple>
+
+
+using namespace std;
+
+
+
+typedef int t_id_user;
+typedef int t_id_item;
+
+
+typedef float t_rating;
+typedef float t_distance;
+typedef float t_similarity;
+typedef list<pair<t_id_item, t_rating> > t_ratings;
+typedef map<t_id_user, t_ratings*> t_rating_matrix;
+typedef map<t_id_user, map<t_similarity, t_id_user>* > t_similarity_matrix;
+
+
+t_distance cosine(t_ratings* a, t_ratings* b){
+
+}
+
+
+class Node;
+
+class RecTree{
+public:
+  RecTree();
+  ~RecTree();
+private:
+  Node* p_root;
+  int max_depth;
+  int max_node;
+};
+
+
+
+
+class Node{
+public:
+  Node();
+  ~Node();
+private:
+  t_ratings centroid;
+  int cur_depth;
+  int max_elements;
+  int cur_elements;
+  Node* p_left;
+  Node* p_right;
+
+};
