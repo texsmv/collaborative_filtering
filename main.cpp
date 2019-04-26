4756d90721b021fb6c50c83757fb0e9f1c7a2fe0
Elena <karengvcs@gmail.com>
2019-04-25 20:22:48 -0500

    lectura db + k_means

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "tipos.h"
+#include "read.h"
 
-int main(int argc, char const *argv[]) {
-  /* code */
+int main(int argc, char const *argv[]){
+      t_rating_vector db(top);
+      readBD("ratings.csv",db);
+      // cout<<"sizedb: "<<db.size()<<endl;
+      print(db);
+  
   return 0;
 }
