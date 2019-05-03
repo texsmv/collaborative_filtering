5e8eb54e4b700b01eefc1bd16b5599f5c7309fac
Alexis <texs.mv@gmail.com>
2019-05-03 00:17:10 -0500

    fix

--- user_based_CB/pruebas.cu
+++ user_based_CB/pruebas.cu
@@ -4,49 +4,15 @@
 #include "distances.h"
 #include "cud_sparse_oper.h"
 #include "recomender.h"
-#include "imgui.h"
-#include "imgui-SFML.h"
 
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
 
 using namespace std;
 
 
 int main(int argc, char const *argv[]) {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
 
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        window.clear();
-        window.draw(shape);
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
 
   int n_ratings, n_users;
   int n_ratings_20, n_users_20, n_ratings_27, n_users_27;
